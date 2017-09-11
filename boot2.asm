f95a0897aa9c52a7daef695d62a24a56d175115f
Heitorado <hscs@cin.ufpe.br>
2017-09-11 14:35:44 -0300

    Start work on boot2.asm

--- boot2.asm
+++ boot2.asm
@@ -1,6 +1,8 @@
 org 0x500
 jmp 0x0000:start
 
+message db 'Loading Adventure...', 13, 10, 0
+
 start:
     xor ax, ax
     mov ds, ax
@@ -32,4 +34,45 @@ load:
 
     jc load     ;se o acesso falhar, tenta novamente
 
+    ;programa começa aqui
+
+    ;Iniciando modo de video
+    mov ah, 0
+    mov al, 12h
+    int 10h
+
+    ;Alterando cor de Fundo
+    mov ah, 0xb
+    mov bh, 0
+    mov bl, 4
+    int 10h
+
+    ;muda cor do texto
+    mov bl, 1
+    ;coloca apontador na mensagem
+    mov si, message
+    call printa_string
+    call hold
+
+    ;programa termina aqui
+
     jmp 0x7e00  ;pula para o setor de endereco 0x7e00 (start do boot2)
+
+
+hold:
+    
+    jmp hold
+
+
+printa_string:
+    lodsb       ;carrega uma letra de si em al e passa para o próximo caractere
+    cmp al, 0   ;chegou no final? (equivalente a um \0)
+    je .done
+ 
+    mov ah, 0eh ;código da instrução para imprimir um caractere que está em al
+    int 10h     ;interrupção de vídeo.
+
+    jmp printa_string ;loop
+
+    .done:   
+        ret
